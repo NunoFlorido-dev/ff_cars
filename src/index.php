b3beb6588be63fb5326bcb0bcea01658292def0d
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2024-11-23 01:58:37 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,10 @@
 <?php
+global $connection;
+
+use PgSql\Result;
+
 session_start();
+include("php/connection.php");
 include("php/definemode.php");
 include("php/userinfo.php");
 include("php/gentools.php");
@@ -15,29 +20,23 @@ if(isset($_POST['button1'])){
     $_SESSION["page"]++;
 }
 
-$values = isset($_GET['search']) ? $_GET['search'] : null;
+$values = $_GET['search'] ?? null;
 $rows = null;
 
-$connection = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
-
-function getData($connection, $values){
+function getData($connection, $values): Result|false|null
+{
 
     if(empty($values)) {
 
         $query = "SELECT * FROM car";
-        try {
-            return pg_query($connection, $query);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
     }else{
         $query = "SELECT * FROM car WHERE brand = '$values'";
 
-        try {
-            return pg_query($connection, $query);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
+    }
+    try {
+        return pg_query($connection, $query);
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
 
     return null;
@@ -47,18 +46,21 @@ $values = getData($connection, $values);
 if(pg_num_rows($values) > 0){
     $rows = pg_fetch_all($values);
 }
-
-
 ?>
-
-    <style>
-        <?php include 'index.css'; ?>
-        <?php include 'css/nav.css'; ?>
-    </style>
-
-
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/nav.css">
+        <link rel="stylesheet" href="css/index.css">
+        <link href="https://fonts.cdnfonts.com/css/zt-talk" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet">
+        <title>FF.Cars | Homepage</title>
+    </head>
     <body>
-
     <nav>
         <div class="top-nav">
             <a class="homepage-link-nav" href="index.php"><img alt="ff.cars logotype" src="../assets/ff_cars_logo.svg" /></a>
@@ -81,20 +83,23 @@ if(pg_num_rows($values) > 0){
 
     <div class="search">
         <form class="search_top" action= "<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="get">
+
+            <label for="name"></label>
             <input class="search_top_textinput" type="text" id="name" name="search" placeholder="Search car..."/>
+
             <div>
                 <button class="search_top_button" type="submit">.</button>
                 <button class="search_top_button">-</button>
             </div>
+
         </form>
+
         <div class="search_bottom">
             <button class="search_bottom_button">Segment</button>
             <button class="search_bottom_button">KM</button>
             <button class="search_bottom_button">NºSeats</button>
             <button class="search_bottom_button">Year</button>
             <button class="search_bottom_button">Gearshift</button>
-        </div>
-        <div class="search_bottom">
             <button class="search_bottom_button">Model</button>
             <button class="search_bottom_button">Fuel Type</button>
             <button class="search_bottom_button">Brand</button>
@@ -103,9 +108,8 @@ if(pg_num_rows($values) > 0){
 
     <div class="car_list">
         <?php
-        function extracted($rows)
+        function extracted($rows): void
         {
-            $text = htmlspecialchars($rows['license_plate']);
             $model = htmlspecialchars($rows['model']);
             $brand = htmlspecialchars($rows['brand']);
             $fuel_type = htmlspecialchars($rows['fuel_type']);
@@ -156,3 +160,4 @@ if(pg_num_rows($values) > 0){
 
 pg_close($connection);
 ?>
+</html>

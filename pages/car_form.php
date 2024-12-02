09e0c0b424725e82684daec0c3af3bca06123833
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2024-12-02 12:21:11 +0000

    Add styling

--- pages/car_form.php
+++ pages/car_form.php
@@ -22,6 +22,7 @@ $email = $_SESSION['email'];
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../assets/css/nav.css">
+    <link rel="stylesheet" href="../assets/css/car_form.css">
     <title>FF.Cars | Car Form</title>
 </head>
 <body>

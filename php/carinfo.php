b3cd3e64d2b3b6294637c297ce5f47a5f684939a
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2024-12-03 17:07:33 +0000

    Include connection

--- php/carinfo.php
+++ php/carinfo.php
@@ -1,4 +1,12 @@
 <?php
+include(__DIR__ . '/../auth/connection.php');
+
+global $connection;
+if (!$connection) {
+    echo "Database connection failed.";
+    exit();
+}
+
 // Ensure that session_start() is only called if the session is not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();  // Start session if not already started

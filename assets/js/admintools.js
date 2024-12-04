bb01f9d00cbc5200b0de01134c2e6a3f987dbce5
Clara Caloy <uc2022208363@student.uc.pt>
2024-12-04 00:49:37 +0000

    return page done

--- assets/js/admintools.js
+++ assets/js/admintools.js
@@ -4,29 +4,37 @@ const buttons = document.querySelectorAll('main .select-page button');
 const generalPage = document.querySelector('main .general-page-cont');
 const carsPage = document.querySelector('main .cars-page-cont');
 
-
 generalPage.classList.add('display-open');
 carsPage.classList.add('display-close');
 
+
+const lastVisitedPage = localStorage.getItem('lastPage');
+if (lastVisitedPage === 'cars') {
+    generalPage.classList.add('display-close');
+    generalPage.classList.remove('display-open');
+
+    carsPage.classList.add('display-open');
+    carsPage.classList.remove('display-close');
+}
+
+
+function switchPage(openPage, closePage, pageName) {
+    openPage.classList.add('display-open');
+    openPage.classList.remove('display-close');
+
+    closePage.classList.add('display-close');
+    closePage.classList.remove('display-open');
+
+    // Salva a página aberta como a última visitada
+    localStorage.setItem('lastPage', pageName);
+}
+
 buttons.forEach((button) => {
-button.addEventListener('click', () => {
-    if(button === generalButton){
-        generalPage.classList.add('display-open');
-        generalPage.classList.remove('display-close');
-
-        carsPage.classList.add('display-close');
-        carsPage.classList.remove('display-open');
-    }
-    if(button === carsButton){
-        carsPage.classList.add('display-open');
-        carsPage.classList.remove('display-close');
-
-        generalPage.classList.add('display-close');
-        generalPage.classList.remove('display-open');
-
-
-
-    }
-});
-
+    button.addEventListener('click', () => {
+        if (button === generalButton) {
+            switchPage(generalPage, carsPage, 'general');
+        } else if (button === carsButton) {
+            switchPage(carsPage, generalPage, 'cars');
+        }
+    });
 });

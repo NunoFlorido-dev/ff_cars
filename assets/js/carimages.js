2582314aecd3582f0c59a6b9d34949cc4d0c7596
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2024-12-04 18:27:44 +0000

    Fix admin tools stuff

--- assets/js/carimages.js
+++ assets/js/carimages.js
@@ -18,3 +18,23 @@ containers.forEach(container => {
 }
 
 displayImages(imageContainers, imageArray);
+
+
+
+const imageContainersAdmin = document.querySelectorAll(".cartool_container .image_container .car_image");
+
+function displayImagesAdmin(containers, images){
+
+    containers.forEach(container => {
+        // Generate a random index from 1 to images.length
+        let random = Math.floor(Math.random() * images.length) + 1;
+
+        // Access the array at (random - 1) since array indices start at 0
+        container.setAttribute('src', images[random - 1]);
+
+        console.log(`Random index: ${random}, Image: ${images[random - 1]}`);
+    });
+
+}
+
+displayImagesAdmin(imageContainersAdmin, imageArray);

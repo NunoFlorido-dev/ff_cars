b724b06b7dfebe8af82a4c24fcd4be3b3eac9f3a
Clara Caloy <uc2022208363@student.uc.pt>
2024-12-07 01:19:24 +0000

    partially added css do add car

--- pages/addcar.php
+++ pages/addcar.php
@@ -22,6 +22,7 @@ $id = fetchID($email);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../assets/css/nav.css">
+    <link rel="stylesheet" href="../assets/css/addcar.css">
     <title>FF.Cars | Add Car</title>
 </head>
 <body>
